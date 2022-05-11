11292b65835b779bf15232465a2f48ea53987dc2
unknown <pedroaugmed7@gmail.com>
2022-05-11 15:25:47 -0300

    add changes heroku

--- src/App.styles.ts
+++ src/App.styles.ts
@@ -1,28 +1,22 @@
 import styled from 'styled-components';
 
-export const Container = styled.div`
-    padding-bottom: 5%;
-    background-color: #FFF;
-    margin: 0;
-    padding: 0;
-    border: 0;
-`;
+export const Container = styled.div``;
 
 export const Header = styled.div`
-    background-color: #2A004F;
-    height: 7em;
+background-color: #2A004F
+    height: 150px;
     text-align: center;
-`;  
+`;
 
 export const HeaderText = styled.h1`
-  margin: 0;
-  padding: 0;
-  color: #00DBAF;
-  padding-top: 30px;
+    margin: 0;
+    padding: 0;
+    color: #FFF;
+    padding-top: 30px;
 `;
 
 export const Body = styled.div`
-  margin-left: 10%;
-  max-width: 980px;
-  text-align: center;
+    margin: auto;
+    max-width: 980px;
+    margin-bottom: 50px;
 `;
