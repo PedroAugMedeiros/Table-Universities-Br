e975c8f0b6000375e228b9875e1dc062811f960e
unknown <pedroaugmed7@gmail.com>
2022-05-11 15:27:30 -0300

    add changes heroku

--- src/App.styles.ts
+++ src/App.styles.ts
@@ -1,22 +1,29 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
-
-export const Header = styled.div`
-background-color: #2A004F
-    height: 150px;
-    text-align: center;
-`;
-
-export const HeaderText = styled.h1`
+export const Container = styled.div`
+    padding-bottom: 5%;
+    background-color: #FFF;
     margin: 0;
     padding: 0;
-    color: #FFF;
-    padding-top: 30px;
+    border: 0;
+`;
+
+export const Header = styled.div`
+    background-color: #2A004F;
+    height: 150px;
+    text-align: center;
+`;  
+
+export const HeaderText = styled.h1`
+  margin: 0;
+  padding: 0;
+  color: #00DBAF;
+  padding-top: 30px;
 `;
 
 export const Body = styled.div`
-    margin: auto;
-    max-width: 980px;
-    margin-bottom: 50px;
+  margin-left: 10%;
+  max-width: 980px;
+  text-align: center;
 `;
+
